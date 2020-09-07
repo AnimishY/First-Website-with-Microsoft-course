64767d288934c1cda1d14bf34b97da1e705be841
AnimishY <animishyadav@outlook.com>
2020-09-07 14:48:17 +0530

    Learning

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 'use strict'
-alert('Hello World');
+alert('Welcome User! Page Under developement');
 console.log('Here\'a hidden message');
 
 let today = new Date ();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+function hide() {
+    document.getElementById("demo").style.display="none";
+}
+
+function show() {
+    document.getElementById("demo").style.display="block";
+}
+
+function printp() {
+    print();
+}
